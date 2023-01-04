7fed813f046d9363e59dd57c3a4abcd93f7280d9
酷酷的辉 <995631664@qq.com>
2023-01-04 21:18:46 +0800

    第5章 误差反向传播法

---
+++ 深度学习/21_gradient_check.py
@@ -0,0 +1,16 @@
+import numpy as np
+from minist数据集.mnist import load_mnist
+from BetterTwoLayerNet import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+    print(key + ' : ' + str(diff))
